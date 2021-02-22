c77b76a6aa2022e3e0d6f67d0af61e5396624620
荒井忠輔 <mr.popo1173@gmail.com>
2021-02-23 08:26:26 +0900

    初回ファイル

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// scroll（別ページからのページ内リンク対応）
+var headerHeight = $('.headerWrap').outerHeight();
+var urlHash = location.hash;
+if(urlHash) {
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function(){
+        var target = $(urlHash);
+        var position = target.offset().top - headerHeight;
+        $('body,html').stop().animate({scrollTop:position}, 500);
+    }, 100);
+}
+$('a[href^="#"]').click(function() {
+    var href= $(this).attr("href");
+    var target = $(href);
+    var position = target.offset().top - headerHeight;
+    $('body,html').stop().animate({scrollTop:position}, 500);
+});
